c3df3ed743ebe6c4bd434d716b8c0136d42ad7bf
Avi190404 <114256809+Avi190404@users.noreply.github.com>
2024-10-16 21:35:14 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <div className='bg-black text-white flex items-center justify-between py-4 px-10'>
-        <div className='text-2xl font-bold cursor-pointer'>DIMS</div>
+        <Link to="/"><div className='text-2xl font-bold cursor-pointer'>DIMS</div></Link>
         {
           isLoggedIn ? (
             <Link to="/profile"><button className='bg-[#eb5d3a] border border-1 border-[rgba(119,119,125,0.2)] px-4 py-2 rounded-md'>Profile</button></Link>
